b0fa52a637c525d29df751d6493599839a6ecd2b
aravindhjayashankar <52237957+aravindhjayashankar@users.noreply.github.com>
2019-07-08 11:54:21 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,9 @@
+
+Number = int(input("Please Enter Number: "))
+Count = 0
+while(Number > 0):
+    Number = Number // 10
+    Count = Count + 1
+
+print("\n Number of Digits in a Given Number = %d" %Count)
+
